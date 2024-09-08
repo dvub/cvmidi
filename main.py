ee657c21b622e7092b09347ca24f5d825302d2d3
dvub <dvubdevs@gmail.com>
2024-09-08 02:33:05 -0700

    add files

---
+++ main.py
@@ -0,0 +1,104 @@
+# $env:OPENCV_LOG_LEVEL = "DEBUG"
+# pipenv run python main.py
+
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import cv2
+import time
+
+import numpy as np
+
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in hand_landmarks
+            ]
+        )
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS,
+            solutions.drawing_styles.get_default_hand_landmarks_style(),
+            solutions.drawing_styles.get_default_hand_connections_style(),
+        )
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(
+            annotated_image,
+            f"{handedness[0].category_name}",
+            (text_x, text_y),
+            cv2.FONT_HERSHEY_DUPLEX,
+            FONT_SIZE,
+            HANDEDNESS_TEXT_COLOR,
+            FONT_THICKNESS,
+            cv2.LINE_AA,
+        )
+
+    return annotated_image
+
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a hand landmarker instance with the video mode:
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path="hand_landmarker.task"),
+    running_mode=VisionRunningMode.VIDEO,
+)
+with HandLandmarker.create_from_options(options) as landmarker:
+    # The landmarker is initialized. Use it here.
+
+    # begin
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # read the current frame
+        _, frame = cap.read()
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+        # TODO:
+        # I am almost certain this isn't the best way to get the current timestamp
+        # fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_timestamp = round(time.time() * 1000)
+
+        result = landmarker.detect_for_video(mp_image, frame_timestamp)
+        annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), result)
+
+        cv2.imshow("Output", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
